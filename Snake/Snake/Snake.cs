ddbfe3a7d9509f5251be34e75bce594874e35dd7
AndarkRA <klushin1973@yandex.ru>
2022-05-11 20:35:20 +0300

    Class snake

---
+++ Snake/Snake/Snake.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    internal class Snake : Figure
+    {
+        public Snake(Point tail, int leght, Direction direction)
+        {
+            plist = new List<Point>();
+            for (int i = 0; i < leght; i++)
+            {
+                Point p = new Point(tail);
+                p.Move(i, direction);
+                plist.Add(p);
+            }
+
+        }
+    }
+}
